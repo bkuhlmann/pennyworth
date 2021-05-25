830e33b22348c92e5b3c978b45293382a9cf23d7
Brooke Kuhlmann <brooke@alchemists.io>
2021-05-25 09:31:47 -0600

    Refactored gemspec to use identity summary
    Necessary to store this information in a central location for use
    within the gemspec and, soon, for printing command line usage
    information.

--- lib/pennyworth/identity.rb
+++ lib/pennyworth/identity.rb
@@ -5,6 +5,7 @@ module Pennyworth
   module Identity
     NAME = "pennyworth"
     LABEL = "Pennyworth"
+    SUMMARY = "A command line interface that augments Alfred workflows."
     VERSION = "10.0.1"
     VERSION_LABEL = "#{LABEL} #{VERSION}"
   end
