d792550c8eb3d07c239c5441ab865625347295ef
Brooke Kuhlmann <brooke@alchemists.io>
2021-05-25 09:38:03 -0600

    Added CLI configuration content
    Necessary to define the content of this gem's configuration.

---
+++ lib/pennyworth/cli/configuration/content.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Pennyworth
+  module CLI
+    module Configuration
+      # Defines configuration content as the primary source of truth for use throughout the gem.
+      Content = Struct.new :action_encodings,
+                           :action_git_hub,
+                           :action_http_statuses,
+                           :action_ruby_gems,
+                           :action_system_errors,
+                           :action_system_signals,
+                           :action_text,
+                           :action_config,
+                           :action_version,
+                           :action_help,
+                           :alfred_preferences,
+                           :inflections,
+                           :git_hub_api_url,
+                           :git_hub_organization,
+                           :git_hub_user,
+                           :http_statuses_url,
+                           :ruby_gems_api_url,
+                           :ruby_gems_owner,
+                           keyword_init: true
+    end
+  end
+end
---
+++ spec/lib/pennyworth/cli/configuration/content_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Pennyworth::CLI::Configuration::Content do
+  subject(:content) { described_class.new }
+
+  describe "#initialize" do
+    let :proof do
+      {
+        action_encodings: nil,
+        action_git_hub: nil,
+        action_http_statuses: nil,
+        action_ruby_gems: nil,
+        action_system_errors: nil,
+        action_system_signals: nil,
+        action_text: nil,
+        action_config: nil,
+        action_version: nil,
+        action_help: nil,
+        alfred_preferences: nil,
+        inflections: nil,
+        git_hub_api_url: nil,
+        git_hub_organization: nil,
+        git_hub_user: nil,
+        http_statuses_url: nil,
+        ruby_gems_api_url: nil,
+        ruby_gems_owner: nil
+      }
+    end
+
+    it "answers default attributes" do
+      expect(content).to have_attributes(proof)
+    end
+  end
+end
