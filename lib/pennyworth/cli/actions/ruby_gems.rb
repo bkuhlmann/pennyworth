1cb99abf90f934a97563bf23deb40dda70b009c5
Brooke Kuhlmann <brooke@alchemists.io>
2021-05-28 11:09:17 -0600

    Added CLI RubyGems action
    Necessary to process RubyGems action input from the command line and
    render the appropriate script filter for use in an Alfred workflow.

---
+++ lib/pennyworth/cli/actions/ruby_gems.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Pennyworth
+  module CLI
+    module Actions
+      # Handles the RubyGems action.
+      class RubyGems
+        def initialize processor: Processor.for_gems, container: Container
+          @processor = processor
+          @container = container
+        end
+
+        def call endpoint
+          processor.call(endpoint)
+                   .to_json
+                   .then { |json| logger.info json }
+        end
+
+        private
+
+        attr_reader :processor, :container
+
+        def logger = container[__method__]
+      end
+    end
+  end
+end
---
+++ spec/lib/pennyworth/cli/actions/ruby_gems_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Pennyworth::CLI::Actions::RubyGems do
+  using Refinements::Structs
+
+  subject(:action) { described_class.new processor: processor }
+
+  include_context "with application container"
+  include_context "with RubyGems integration"
+
+  let(:processor) { instance_spy Pennyworth::Processor, call: [ruby_gems_record] }
+
+  describe "#call" do
+    it "calls processor with API endpoint" do
+      action.call "owners/test/gems.json"
+      expect(processor).to have_received(:call).with("owners/test/gems.json")
+    end
+
+    it "answers JSON" do
+      results = proc { action.call "owners/test/gems.json" }
+      expect(&results).to output("#{[ruby_gems_record].to_json}\n").to_stdout
+    end
+  end
+end
