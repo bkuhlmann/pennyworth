3f7bab72aba83115ef05b82e67af060cecc5472a
Brooke Kuhlmann <brooke@alchemists.io>
2021-05-28 16:22:20 -0600

    Added RSpec GitHub shared context
    Necessary to extract common objects that will soon be used across
    multiple specs.

---
+++ spec/support/shared_contexts/git_hub_integration.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+RSpec.shared_context "with GitHub integration" do
+  let :git_hub_record do
+    {
+      name: "test",
+      html_url: "https://github.com/bkuhlmann/test",
+      description: "A test project.",
+      homepage: "https://www.alchemists.io/projects/test"
+    }
+  end
+end
