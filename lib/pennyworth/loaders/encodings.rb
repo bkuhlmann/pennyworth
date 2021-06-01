0fa826f4b55c77221003cc2d13493f0bb8f5d4f5
Brooke Kuhlmann <brooke@alchemists.io>
2021-06-01 16:31:35 -0600

    Added encodings loader
    Necessary to load initial list of encoding records for further
    processing.

---
+++ lib/pennyworth/loaders/encodings.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require "refinements/arrays"
+
+module Pennyworth
+  module Loaders
+    # Loads an array of encoding records.
+    class Encodings
+      using Refinements::Arrays
+
+      def initialize encoding: Encoding, model: Models::Encoding
+        @encoding = encoding
+        @model = model
+      end
+
+      def call _omit = nil
+        encoding.aliases
+                .map { |_second, first| first }
+                .uniq
+                .sort
+                .map do |first|
+                  model[name: first, aliases: encoding.find(first).names.sort.excluding(first)]
+                end
+      end
+
+      private
+
+      attr_reader :encoding, :model
+    end
+  end
+end
