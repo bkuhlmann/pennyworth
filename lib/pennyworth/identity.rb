0eb031e6d024c62bfa4c8caa6246264886782328
Brooke Kuhlmann <brooke@alchemists.io>
2022-01-01 11:08:28 -0700

    Added version release notes

--- lib/pennyworth/identity.rb
+++ lib/pennyworth/identity.rb
@@ -6,7 +6,7 @@ module Pennyworth
     NAME = "pennyworth"
     LABEL = "Pennyworth"
     SUMMARY = "A command line interface that augments Alfred workflows."
-    VERSION = "12.0.0"
+    VERSION = "12.0.1"
     VERSION_LABEL = "#{LABEL} #{VERSION}".freeze
   end
 end
