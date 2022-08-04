9ef5b24eb7a9cda2e46fe17e17a2360e30b58059
Brooke Kuhlmann <brooke@alchemists.io>
2022-08-03 20:40:08 -0600

    Removed registration of duplicate keys within containers
    This behavior is unnecessary for merging containers and removes the
    possiblity of unnecessary mutation of registered objects within the
    container.

--- lib/pennyworth/cli/actions/container.rb
+++ lib/pennyworth/cli/actions/container.rb
@@ -9,8 +9,6 @@ module Pennyworth
       module Container
         extend Dry::Container::Mixin
 
-        config.registry = ->(container, key, value, _options) { container[key.to_s] = value }
-
         merge Pennyworth::Container
 
         register(:config) { Config.new }
