9c06d892a26c5a6d49b6ff68abed5871ad3d7de9
Brooke Kuhlmann <brooke@alchemists.io>
2023-04-19 17:24:19 -0600

    Removed duplicated code from Sod upgrade
    None of this is necessary and can be safely deleted now that this project has been updated to run on Sod.

--- spec/support/shared_examples/a_parser.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.shared_examples "a parser" do
-  describe ".call" do
-    it "answers empty array" do
-      expect(described_class.call).to be_a(Pennyworth::Configuration::Content)
-    end
-  end
-end
