04b8548de40d95681a533d2bee9c1e54642508f6
Brooke Kuhlmann <brooke@alchemists.io>
2023-05-03 16:18:57 -0600

    Updated container to use Etcher configuration
    Necessary to leverage Etcher and Runcom for loading and resolving the configuration.

--- lib/pennyworth/container.rb
+++ lib/pennyworth/container.rb
@@ -12,9 +12,19 @@ module Pennyworth
   module Container
     extend Dry::Container::Mixin
 
-    register(:configuration) { Configuration::Loader.call }
+    register :configuration do
+      self[:defaults].add_loader(Etcher::Loaders::YAML.new(self[:xdg_config].active))
+                     .then { |registry| Etcher.call registry }
+    end
+
+    register :defaults do
+      Etcher::Registry.new(contract: Configuration::Contract, model: Configuration::Model)
+                      .add_loader(Etcher::Loaders::YAML.new(self[:defaults_path]))
+    end
+
+    register(:defaults_path) { Pathname(__dir__).join("configuration/defaults.yml") }
+    register(:xdg_config) { Runcom::Config.new "pennyworth/configuration.yml" }
     register(:specification) { Spek::Loader.call "#{__dir__}/../../pennyworth.gemspec" }
-    register(:environment) { ENV }
     register(:kernel) { Kernel }
     register(:http) { HTTP }
     register(:logger) { Cogger.new formatter: :emoji }
--- spec/support/shared_contexts/application_dependencies.rb
+++ spec/support/shared_contexts/application_dependencies.rb
@@ -6,7 +6,12 @@ require "infusible/stub"
 RSpec.shared_context "with application dependencies" do
   using Infusible::Stub
 
-  let(:configuration) { Pennyworth::Configuration::Loader.with_defaults.call }
+  let :configuration do
+    Etcher.new(Pennyworth::Container[:defaults])
+          .call(git_hub_organization: "acme", git_hub_user: "test", ruby_gems_owner: "test")
+          .bind(&:dup)
+  end
+
   let(:kernel) { class_spy Kernel }
   let(:logger) { Cogger.new io: StringIO.new, formatter: :emoji }
 
