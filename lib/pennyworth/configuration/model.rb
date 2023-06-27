5e24d50fae3062d4f325e2442cecb91ee472ed6c
Brooke Kuhlmann <brooke@alchemists.io>
2023-06-27 11:01:41 -0600

    Removed configuration model freezing
    Redundant since the Etcher gem takes care of this for us.
    
    Release: minor
    Format: asciidoc

--- lib/pennyworth/configuration/model.rb
+++ lib/pennyworth/configuration/model.rb
@@ -3,20 +3,13 @@
 module Pennyworth
   module Configuration
     # Defines configuration content as the primary source of truth for use throughout the gem.
-    Model = Struct.new(
-      :alfred_preferences,
-      :inflections,
-      :git_hub_api_url,
-      :git_hub_organization,
-      :git_hub_user,
-      :http_statuses_url,
-      :ruby_gems_api_url,
-      :ruby_gems_owner
-    ) do
-      def initialize(**)
-        super
-        freeze
-      end
-    end
+    Model = Struct.new :alfred_preferences,
+                       :inflections,
+                       :git_hub_api_url,
+                       :git_hub_organization,
+                       :git_hub_user,
+                       :http_statuses_url,
+                       :ruby_gems_api_url,
+                       :ruby_gems_owner
   end
 end
