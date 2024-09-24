5eeedd56cc7af67006d01c1ca987adfb10d5efaa
Brooke Kuhlmann <brooke@alchemists.io>
2024-09-24 10:01:33 -0600

    Added htmx model
    Necessary to represent a link from the link:https://htmx.org[htmx] web site.
    
    Milestone: minor

--- lib/pennyworth.rb
+++ lib/pennyworth.rb
@@ -5,7 +5,8 @@ require "zeitwerk"
 Zeitwerk::Loader.new.then do |loader|
   loader.inflector.inflect "cli" => "CLI",
                            "http_status" => "HTTPStatus",
-                           "http_statuses" => "HTTPStatuses"
+                           "http_statuses" => "HTTPStatuses",
+                           "htmx" => "HTMX"
   loader.tag = File.basename __FILE__, ".rb"
   loader.push_dir __dir__
   loader.setup
---
+++ lib/pennyworth/models/htmx.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Pennyworth
+  module Models
+    # Represents a htmx link.
+    HTMX = Data.define :label, :description, :uri
+  end
+end
