40dab2eaf7f4a6e98bf12fc265da709bb0854bec
Brooke Kuhlmann <brooke@alchemists.io>
2024-10-07 09:02:47 -0600

    Added CLI htmx extensions action
    Necessary to process and output Alfred script filter JSON for htmx link:https://htmx.org/extensions[Extensions].
    
    Milestone: minor

---
+++ lib/pennyworth/cli/actions/htmx/extension.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require "sod"
+
+module Pennyworth
+  module CLI
+    module Actions
+      module HTMX
+        # Handles extension documentation action.
+        class Extension < Sod::Action
+          include Import[:settings, :io]
+
+          description "Render Alfred extensions script filter."
+
+          on "--extensions"
+
+          def initialize(processor: Processor.for_htmx, **)
+            @processor = processor
+            super(**)
+          end
+
+          def call(*) = io.puts processor.call(settings.htmx_extensions_uri).to_json
+
+          private
+
+          attr_reader :processor
+        end
+      end
+    end
+  end
+end
